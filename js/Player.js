2fe711b20c940762a9c94185bc7d76306990b369
JoshuaPetherick <peth@live.co.uk>
2016-11-09 17:43:04 +0000

    Added on-screen buttons! Only got left and right working, need to adjustable the others. Means structure of player has and will need further re-design

--- js/Player.js
+++ js/Player.js
@@ -12,51 +12,89 @@ function playerInit(x, y, h, w) {
     foreground.add(player);
 }
 
-function playerInput(player) {
-    var speed = 2;
+function inputInit() {
+    var leftButton = game.add.button(10, GAMEHEIGHT-40,'leftArrow', null, this, 0, 1, 0, 1);
+    leftButton.events.onInputOver.add(function(){leftKey = true;});
+    leftButton.events.onInputOut.add(function(){leftKey = false;});
+    leftButton.events.onInputDown.add(function(){leftKey = true;});
+    leftButton.events.onInputUp.add(function(){leftKey = false;});
 
-    //Handle Left
-    if (leftKey.isDown) {
-        //console.log("Pressed Left: " + player.x);
-        player.x = player.x - speed;
-        if (player.x <= 0) { // Check not going off the screen!
-            player.x = player.x + speed;
-        }
-        else if (player.state != playerStates.FALLING) { // Make sure still on a platform
-            if( playerCheckCollision(floors) == false && playerCheckCollision(stairs) == false) {
-                player.state = playerStates.FALLING;
-            }
+    var rightButton = game.add.button(GAMEWIDTH-60, GAMEHEIGHT-40, 'rightArrow', null, this, 0, 1, 0, 1);;
+    rightButton.events.onInputOver.add(function(){rightKey = true;});
+    rightButton.events.onInputOut.add(function(){rightKey = false});
+    rightButton.events.onInputDown.add(function(){rightKey = true});
+    rightButton.events.onInputUp.add(function(){rightKey = false});
+
+    var upButton = game.add.button(GAMEWIDTH-110, GAMEHEIGHT-50, 'upArrow', null, this, 0, 1, 0, 1);
+    //upButton.events.onInputOver.add();
+    //upButton.events.onInputDown.add();
+
+    var downButton = game.add.button(70, GAMEHEIGHT-50, 'downArrow', null, this, 0, 1, 0, 1);
+    //downButton.events.onInputOver.add();
+    //downButton.events.onInputDown.add();
+
+    var spaceButton = game.add.button((GAMEWIDTH/2)-50, GAMEHEIGHT-40, 'spaceBar', null, this, 0, 1, 0, 1);
+    //spaceButton.events.onInputOver.add();
+    //spaceButton.events.onInputDown.add();
+}
+
+function moveLeft() {
+    player.x = player.x - playerSpeed;
+    if (player.x <= 0) { // Check not going off the screen!
+        player.x = player.x + playerSpeed;
+    }
+    else if (player.state != playerStates.FALLING) { // Make sure still on a platform
+        if( playerCheckCollision(floors) == false && playerCheckCollision(stairs) == false) {
+            player.state = playerStates.FALLING;
         }
     }
-    // Handle Right
-    if (rightKey.isDown) {
-        //console.log("Pressed Right: " + player.x);
-        player.x = player.x + speed;
-        if (player.x >= GAMEWIDTH) { // Check not going off the screen!
-            player.x = player.x - speed;
+}
+
+function moveRight() {
+    player.x = player.x + playerSpeed;
+    if (player.x >= GAMEWIDTH) { // Check not going off the screen!
+        player.x = player.x - playerSpeed;
+    }
+    else if (player.state != playerStates.FALLING) { // Make sure still on a platform
+        if (playerCheckCollision(floors) == false && playerCheckCollision(stairs) == false) {
+            player.state = playerStates.FALLING;
         }
-        else if (player.state != playerStates.FALLING) { // Make sure still on a platform
-            if (playerCheckCollision(floors) == false && playerCheckCollision(stairs) == false) {
-                player.state = playerStates.FALLING;
-            }
-        }
+    }
+}
+
+function playerInput(player) {
+
+    if (leftKey == true) {
+        moveLeft();
+    }
+    if (rightKey == true) {
+        moveRight();
+    }
+    if (downKey == true) {
+
+    }
+    if (upKey == true) {
+
+    }
+    if (spaceBar == true) {
+
     }
     // Handle Up
-    if (upKey.isDown) {
-        if (playerCheckCollision(stairs)) {
-            player.y = player.y - speed;
-        }
-    }
-    // Handle Down
-    if (downKey.isDown) {
-        if (playerCheckCollision(stairs) && playerCheckCollision(floors) == false) {
-            player.y = player.y + speed;
-        }
-    }
-    // Handling jumping
-    if (spaceBar.isDown && player.state != playerStates.FALLING) {
-        yell.play();
-    }
+    // if (upKey.isDown) {
+    //     if (playerCheckCollision(stairs)) {
+    //         player.y = player.y - speed;
+    //     }
+    // }
+    // // Handle Down
+    // if (downKey.isDown) {
+    //     if (playerCheckCollision(stairs) && playerCheckCollision(floors) == false) {
+    //         player.y = player.y + speed;
+    //     }
+    // }
+    // // Handling jumping
+    // if (spaceBar.isDown && player.state != playerStates.FALLING) {
+    //     yell.play();
+    // }
 }
 
 function playerUpdate() {
